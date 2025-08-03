66f6d825ecebdb0ec53ee50d13fad4acc4127531
robertWalker68501 <robert.walker6580@gmail.com>
2025-08-02 19:48:23 -0700

    fixed clerk error

--- components/shared/navigation/Navbar.tsx
+++ components/shared/navigation/Navbar.tsx
@@ -3,13 +3,8 @@
 import SiteLogo from '@/components/shared/SiteLogo';
 import MobileNav from '@/components/shared/navigation/MobileNav';
 import DesktopNav from '@/components/shared/navigation/DesktopNav';
-import { useUser } from '@clerk/nextjs';
 
 const Navbar = () => {
-  const { user } = useUser();
-
-  console.log('UserInfo', user);
-
   return (
     <nav className='flex items-center justify-between'>
       <SiteLogo
